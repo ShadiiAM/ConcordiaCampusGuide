4f3600ac8be034d9d61099c0e45440ffbfe5a7d1
Ren-cc <122772170+Ren-cc@users.noreply.github.com>
2026-02-12 23:05:31 -0500

    Added Tests for RoutesRepository.kt

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -203,6 +203,8 @@ dependencies {
     testImplementation("org.robolectric:robolectric:4.12.2")
     testImplementation("androidx.test:core:1.6.1")
     testImplementation("junit:junit:4.13.2")
+    testImplementation("com.squareup.okhttp3:mockwebserver:4.12.0")
+    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.7.3")
 
 
     implementation(libs.play.services.maps.v1820)
