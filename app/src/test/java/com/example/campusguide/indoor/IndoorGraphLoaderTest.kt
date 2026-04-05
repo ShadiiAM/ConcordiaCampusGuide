ac94d21e85cdb7948dd59f27d51ae294ebc972cd
Omar Chabti <72052477+Arzulun@users.noreply.github.com>
2026-04-05 01:39:46 -0400

    Added more IndoorGraphLoaderTests for higher test coverage

--- app/src/test/java/com/example/campusguide/indoor/IndoorGraphLoaderTest.kt
+++ app/src/test/java/com/example/campusguide/indoor/IndoorGraphLoaderTest.kt
@@ -15,6 +15,32 @@ import java.io.ByteArrayInputStream
 class IndoorGraphLoaderTest {
 
     @Test
+    fun `parse maps every known drawable name to resource id`() {
+        val cases = listOf(
+            "cc_floor_1" to R.drawable.cc_floor_1,
+            "hall_floor_1" to R.drawable.hall_floor_1,
+            "hall_floor_2" to R.drawable.hall_floor_2,
+            "hall_floor_8" to R.drawable.hall_floor_8,
+            "hall_floor_9" to R.drawable.hall_floor_9,
+            "lb_floor_2" to R.drawable.lb_floor_2,
+            "lb_floor_3" to R.drawable.lb_floor_3,
+            "lb_floor_4" to R.drawable.lb_floor_4,
+            "lb_floor_5" to R.drawable.lb_floor_5,
+            "molson_floor_1" to R.drawable.molson_floor_1,
+            "molson_floor_s2" to R.drawable.molson_floor_s2,
+            "ve_floor_1" to R.drawable.ve_floor_1,
+            "ve_floor_2" to R.drawable.ve_floor_2,
+            "vl_floor_1" to R.drawable.vl_floor_1,
+            "vl_floor_2" to R.drawable.vl_floor_2
+        )
+
+        cases.forEach { (imageBaseName, expectedRes) ->
+            val graph = IndoorGraphLoader.parse(minimalGraphJson(imageName = "$imageBaseName.png"))
+            assertEquals("Failed on drawable '$imageBaseName'", expectedRes, graph.floorPlanDrawableRes)
+        }
+    }
+
+    @Test
     fun `parse maps known drawable name to resource id`() {
         val graph = IndoorGraphLoader.parse(minimalGraphJson(imageName = "hall_floor_1.png"))
 
@@ -28,6 +54,13 @@ class IndoorGraphLoaderTest {
         val graph = IndoorGraphLoader.parse(minimalGraphJson(imageName = "does_not_exist.png"))
 
         assertEquals(0, graph.floorPlanDrawableRes)
+    }
+
+    @Test
+    fun `parse supports image names without extension`() {
+        val graph = IndoorGraphLoader.parse(minimalGraphJson(imageName = "hall_floor_8"))
+
+        assertEquals(R.drawable.hall_floor_8, graph.floorPlanDrawableRes)
     }
 
     @Test
