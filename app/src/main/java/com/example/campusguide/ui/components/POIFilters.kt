e26fd53705461061fcefc127443d53d4c5c68335
hossamgasser <hossgasser@gmail.com>
2026-04-05 21:21:12 -0400

    Update POIFilters.kt

--- app/src/main/java/com/example/campusguide/ui/components/POIFilters.kt
+++ app/src/main/java/com/example/campusguide/ui/components/POIFilters.kt
@@ -79,6 +79,11 @@ fun POIFilterTags(
                     verticalAlignment = Alignment.CenterVertically,
                     modifier = Modifier.padding(vertical = 6.dp, horizontal = 12.dp)
                 ) {
+                    AccessibleText(
+                        text = poiFilters.rating.toInt().toString() + " ",
+                        baseFontSizeSp = 14f,
+                        modifier = Modifier.padding(start = 6.dp)
+                    )
                     Icon(
                         painter = painterResource(R.drawable.rating_star),
                         contentDescription = "rating_POI_filter",
@@ -86,7 +91,7 @@ fun POIFilterTags(
                         tint = Color(0xFF000000)
                     )
                     AccessibleText(
-                        text = poiFilters.rating.toInt().toString() + " <",
+                        text = "+",
                         baseFontSizeSp = 14f,
                         modifier = Modifier.padding(start = 6.dp)
                     )
@@ -116,8 +121,8 @@ fun POIFilterTags(
                     )
                     AccessibleText(
                         text = when {
-                            poiFilters.distanceLimit < 1000.0f -> "${poiFilters.distanceLimit.toInt()} m >"
-                            poiFilters.distanceLimit % 1000.0f == 0.0f -> "${(poiFilters.distanceLimit / 1000).toInt()} km >"
+                            poiFilters.distanceLimit < 1000.0f -> "${poiFilters.distanceLimit.toInt()} m"
+                            poiFilters.distanceLimit % 1000.0f == 0.0f -> "Less than ${(poiFilters.distanceLimit / 1000).toInt()} km away"
                             else -> "shouldn't reach here"
                         },
                         baseFontSizeSp = 14f,
